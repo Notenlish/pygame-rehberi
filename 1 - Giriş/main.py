94da17c39f7439e7acbd8ae94e2daaba004a9509
Notenlish <71970100+Notenlish@users.noreply.github.com>
2025-02-21 00:32:55 +0300

    Update main.py

--- 1 - Giriş/main.py
+++ 1 - Giriş/main.py
@@ -6,9 +6,6 @@ pygame.init()
 # ekranı oluştur(genişlik, yükseklik)
 screen = pygame.display.set_mode([640, 480])
 
-surf = pygame.Surface((100, 100)).convert_alpha()
-# pygame.draw.circle(surf, pygame.Color(255, 0, 0, 125), (50, 50), 50)
-surf.fill(pygame.Color(255,0,0,125))
 
 # zamanlayıcı(fps'i 60 a sabitlemek için)
 clock = pygame.time.Clock()
@@ -22,9 +19,7 @@ while True:
 
     # ekranı maviye boya
     # RGB, hex veya ingilizce kelime
-    screen.fill("black")
-
-    screen.blit(surf, (50, 50))
+    screen.fill("blue")
 
     # ekranı yenile
     pygame.display.flip()
