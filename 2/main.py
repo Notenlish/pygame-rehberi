bc7324fa300a9b6420b8f427c3d4610ef6aac680
Notenlish <71970100+Notenlish@users.noreply.github.com>
2025-02-21 16:25:27 +0300

    Update main.py

--- 2/main.py
+++ 2/main.py
@@ -1,8 +1,7 @@
 import pygame
 
-
 class Game:
-    def __init__(self) -> None:
+    def __init__(self):
         # pygame'i yükle
         pygame.init()
 
@@ -12,16 +11,14 @@ class Game:
         # zamanlayıcı(fps'i 60 a sabitlemek için)
         self.clock = pygame.time.Clock()
 
-        # karakterin görseli
-        self.surface = pygame.image.load("player.png").convert_alpha()
-        self.pos: list[float] = [100, 300]  # karakter konumu(x, y)
+        # oyuncunun sprite'ı(görseli)
+        self.player_surface = pygame.image.load("player.png")
+        self.player_pos = [100, 300]  # karakterin konumu(x, y)
 
         # çarpışma(collision) için alan
         self.collision_area = pygame.Rect(50, 50, 200, 100)
 
     def run(self):
-        dt = 0
-
         # ana uygulama döngüsü
         while True:
             for event in pygame.event.get():
@@ -31,37 +28,38 @@ class Game:
             keys = pygame.key.get_pressed()
 
             if keys[pygame.K_UP]:
-                self.pos[1] -= 200 * dt
+                self.player_pos[1] -= 200 * dt
             elif keys[pygame.K_DOWN]:
-                self.pos[1] += 200 * dt
-
-            if keys[pygame.K_LEFT]:
-                self.pos[0] -= 200 * dt
-            elif keys[pygame.K_RIGHT]:
-                self.pos[0] += 200 * dt
-
-            player_rect = pygame.Rect(*self.pos, *self.surface.size)  # type:ignore
+                self.player_pos[1] += 200 * dt
+            
+            if keys[pygame.K_RIGHT]:
+                self.player_pos[0] += 200 * dt
+            elif keys[pygame.K_LEFT]:
+                self.player_pos[0] -= 200 * dt
+            
+            # (x,y, genişlik, yükseklik)
+            player_rect = pygame.Rect(
+                *self.player_pos,
+                *self.player_surface.size)
 
             area_color = (219, 107, 42)
-            if player_rect.colliderect(self.collision_area):
-                area_color = (214, 59, 23)
 
-            # ekranı maviye boya
-            # RGB, hex veya ingilizce kelime
+            if player_rect.colliderect(self.collision_area):
+                area_color = (214, 57, 23)
+            
             self.screen.fill((42, 157, 219))
 
-            # alanı çiz
-            pygame.draw.rect(self.screen, area_color, self.collision_area)
+            pygame.draw.rect(
+                self.screen, area_color, self.collision_area
+            )
 
-            # karakteri çiz
-            self.screen.blit(self.surface, self.pos)
+            self.screen.blit(self.player_surface, self.player_pos)
 
             # ekranı yenile
             pygame.display.flip()
 
-            # 144 fps'e sabitle
-            dt = self.clock.tick(144) / 1000
-
+            # 60 fps'e sabitle
+            dt = self.clock.tick(60) / 1000
 
 game = Game()
 game.run()
